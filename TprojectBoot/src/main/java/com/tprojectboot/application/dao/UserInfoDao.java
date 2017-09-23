f2d1847a38d7c213a6e2db728d09412cf0de3430
seochangwook <scw05313315@gmail.com>
2017-09-23 13:31:31 +0900

    spring jpa, hibernate fix - 2

--- TprojectBoot/src/main/java/com/tprojectboot/application/dao/UserInfoDao.java
+++ TprojectBoot/src/main/java/com/tprojectboot/application/dao/UserInfoDao.java
@@ -7,12 +7,16 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tprojectboot.application.model.UserInfo;
 
 //해당 Model에 맞는 Dao가 필요. 해당 Dao에서는 관련 테이블에 대한 CRUD작업을 하는 메소드 생성//
 //XML작업 필요없이 효과적으로 쿼리맵핑 및 JDBC로직 구성//
+//JpaRepository<>를 사용해도 된다. 차이는 CrudRepository에는 없는 페이징 처리, 배치작업 등의 메소드들이 있다.//
+//CrudRepository가 더 최상위의 인터페이스. CrudRepository<테이블명, 테이블의 Id(Primary Key) Type>//
+@Repository
 public interface UserInfoDao extends CrudRepository<UserInfo, String>{
 	List<UserInfo> findAll();
 	
